f3064a2f5a0ac7e117ea0b54c5d89cc222156b15
Ro Ngoc Vo <ngocro208@gmail.com>
2021-12-04 20:04:15 +0700

    Chore: Adjust cron job time

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -17,13 +17,14 @@ describe("Strava", () => {
 		// Kudos
 		cy.scrollTo("bottom", { duration: 1000 }).then(() => {
 			const unfillKudoButtonSelector = "[data-testid=unfilled_kudos]";
+			const STRAVA_ATHLETE_PREFIX = "https://www.strava.com/athletes/";
 			if (Cypress.$(unfillKudoButtonSelector).length > 0) {
 				cy.get(unfillKudoButtonSelector).each(($el, index, $list) => {
 					cy.wrap($el)
 						.closest(".react-feed-component")
 						.within(() => {
 							cy.get('a[data-testid="owners-name"]').then(($owner) => {
-								const ownerId = $owner.prop("href");
+								const ownerId = $owner.prop("href")?.split(STRAVA_ATHLETE_PREFIX)[1];
 								if (ownerId !== Cypress.env("STRAVA_ATHLETE_ID")) {
 									cy.log(`Kudo to ${$owner.text()}'s activity'`);
 									cy.wrap($el).should("exist").click({ force: true });
