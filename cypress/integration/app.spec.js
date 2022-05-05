63e234513990a6f6a7c6d92044d902d9bb1e25b7
Ro VN <ngocro208@gmail.com>
2022-05-05 12:09:52 +0700

    chore: remove ATHLETE_ID env

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -16,22 +16,29 @@ describe("Strava", () => {
 		// Kudos
 		cy.scrollTo("bottom", { duration: 1000 }).then(() => {
 			const unfillKudoButtonSelector = "[data-testid=unfilled_kudos]";
-			const STRAVA_ATHLETE_PREFIX = "https://www.strava.com/athletes/";
+			const profileSelector = "#athlete-profile .card-body > a";
+
 			if (Cypress.$(unfillKudoButtonSelector).length > 0) {
-				cy.get(unfillKudoButtonSelector).each(($el, index, $list) => {
-					cy.wrap($el)
-						.closest(".react-feed-component")
-						.within(() => {
-							cy.get('a[data-testid="owners-name"]').then(($owner) => {
-								const ownerId = $owner.prop("href")?.split(STRAVA_ATHLETE_PREFIX)[1];
-								if (ownerId !== Cypress.env("STRAVA_ATHLETE_ID")) {
-									cy.log(`Kudo to ${$owner.text()}'s activity'`);
-									cy.wrap($el).should("exist").click({ force: true });
-								}
-							});
+				cy.get(profileSelector)
+					.invoke("attr", "href")
+					.then((profileHref) => {
+						cy.get(unfillKudoButtonSelector).each(($el) => {
+							cy.wrap($el)
+								.closest("[data-testid=web-feed-entry]")
+								.within(() => {
+									cy.get('a[data-testid="owners-name"]')
+										.invoke("attr", "href")
+										.then((ownerHref) => {
+											if (getAthleteId(ownerHref) !== getAthleteId(profileHref)) {
+												cy.wrap($el).should("exist").click({ force: true });
+											}
+										});
+								});
 						});
-				});
+					});
 			}
 		});
 	});
 });
+
+const getAthleteId = (href) => href.split("/athletes/")[1];
